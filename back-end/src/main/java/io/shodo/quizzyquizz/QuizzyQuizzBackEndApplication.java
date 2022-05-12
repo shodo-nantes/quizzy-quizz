c5f93d4a7f2e90a18e338c9e0f8bba877da61c84
Thomas Carpaye <thomas.carpaye@gmail.com>
2022-05-12 16:35:42 +0200

    [QZQ-001] Add some spring :'(

---
+++ back-end/src/main/java/io/shodo/quizzyquizz/QuizzyQuizzBackEndApplication.java
@@ -0,0 +1,13 @@
+package io.shodo.quizzyquizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzyQuizzBackEndApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QuizzyQuizzBackEndApplication.class, args);
+  }
+
+}
---
+++ back-end/src/test/java/io/shodo/quizzyquizz/QuizzyQuizzBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shodo.quizzyquizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzyQuizzBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

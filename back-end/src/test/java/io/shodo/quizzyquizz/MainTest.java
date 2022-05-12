151b566e7e14db87cd26039ca36c8741b7aebe6d
Florine Cerclé <florine.cercle@gmail.com>
2022-05-12 12:11:40 +0200

    [QZQ-001] create first test

---
+++ back-end/src/test/java/io/shodo/quizzyquizz/MainTest.java
@@ -0,0 +1,13 @@
+package io.shodo.quizzyquizz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void when_a_new_game_is_created_a() {
+
+    }
+}

ad03f3c20e898bbcbc6aedabda858aee5a425973
Thomas Carpaye <thomas.carpaye@gmail.com>
2022-05-12 12:34:49 +0200

    [QZQ-001] when_a_new_game_is_created_a_question_should_be_returned

--- back-end/src/test/java/io/shodo/quizzyquizz/MainTest.java
+++ back-end/src/test/java/io/shodo/quizzyquizz/MainTest.java
@@ -2,12 +2,18 @@ package io.shodo.quizzyquizz;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MainTest {
 
     @Test
-    void when_a_new_game_is_created_a() {
+    void when_a_new_game_is_created_a_question_should_be_returned() {
+        assertThat(new Game("new game").question()).isEqualTo("List the Charlize Theron movies");
+    }
 
+    private record Game(String s) {
+        private String question() {
+            return "List the Charlize Theron movies";
+        }
     }
 }

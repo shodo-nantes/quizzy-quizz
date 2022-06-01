6f74de547fc51120f348442e2fab6a0fc23e72af
Thomas Carpaye <thomas.carpaye@gmail.com>
2022-06-01 12:36:59 +0200

    [QZQ-001] fix compilation

--- back-end/src/test/java/io/shodo/quizzyquizz/infra/providers/TheMovieDBAnswersProviderTestResponse.java
+++ back-end/src/test/java/io/shodo/quizzyquizz/infra/providers/TheMovieDBAnswersProviderTestResponse.java
@@ -4,7 +4,9 @@ import io.shodo.quizzyquizz.domain.Answers;
 import io.shodo.quizzyquizz.domain.Movie;
 import io.shodo.quizzyquizz.domain.Question;
 import io.shodo.quizzyquizz.domain.Question.QuestionType;
+import io.shodo.quizzyquizz.infra.adapters.TheMovieDBAdapter;
 import io.shodo.quizzyquizz.infra.adapters.TheMovieDBAnswersProvider;
+import io.shodo.quizzyquizz.infra.fakes.FakeTheMovieDBClient;
 import io.shodo.quizzyquizz.infra.rest.Answer;
 import io.shodo.quizzyquizz.infra.rest.AnswersResponse;
 import org.junit.jupiter.api.Test;
@@ -15,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TheMovieDBAnswersProviderTestResponse {
 
-    private final TheMovieDBAnswersProvider theMovieDBAnswersProvider = new TheMovieDBAnswersProvider(new FakeMovieDBService());
+    private final TheMovieDBAnswersProvider theMovieDBAnswersProvider = new TheMovieDBAnswersProvider(new TheMovieDBAdapter(new FakeTheMovieDBClient()));
 
     @Test
     void should_answer_list_of_movies_when_calling_getAnswersFor_with_a_question_of_type_MOVIES_THE_ACTOR_PLAYED_IN() {

4cc22f0849179c8d5614ed52674a4b9426943f6b
jpiazzal <joel.piazzalungalerat@gmail.com>
2023-04-18 15:01:48 +0200

    feat(backend): Add dotenv to be able to use environments variables in application

--- packages/backend/README.md
+++ packages/backend/README.md
@@ -1,3 +1,13 @@
 # Quizzy quizz backend
 
 Backend for quizzy quizz app
+
+## Configuration
+
+You can configure the backend by creating a `.env` file in the root of the backend package.
+
+### Environment variables
+
+| Name | Description               | Default value |
+| ---- | ------------------------- | ------------- |
+| PORT | Port to run the server on | 3000          |
--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,12 +1,15 @@
+import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
 
+dotenv.config();
+
 const app: Express = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (request: Request, result: Response) => {
     result.send('Hello world!');
 });
 
-app.listen(port, () => {
-    console.info(`Listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.info(`Listening at http://localhost:${PORT}`);
 });

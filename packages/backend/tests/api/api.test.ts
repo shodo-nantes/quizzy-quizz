c30c6368415ace550058c96953cf0e1aa8e416a1
jpiazzal <joel.piazzalungalerat@gmail.com>
2023-04-18 18:09:26 +0200

    refactor(backend): Use constants for api path in api tests

--- packages/backend/tests/api/api.test.ts
+++ packages/backend/tests/api/api.test.ts
@@ -1,9 +1,11 @@
 import app from 'app';
 import request from 'supertest';
 
+import { API_VERSION, API_BASE_ROUTE } from 'constants/ApiConstants';
+
 describe('API', () => {
     it('should return the API version', async () => {
-        const result = await request(app).get('/api/v1').expect(200);
-        expect(result.body).toEqual({ version: 1 });
+        const result = await request(app).get(API_BASE_ROUTE).expect(200);
+        expect(result.body).toEqual({ version: API_VERSION });
     });
 });

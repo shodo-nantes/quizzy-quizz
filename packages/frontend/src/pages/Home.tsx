0da31db9c8e37ab205b3508ceb8e4e0e6fabe2a2
jpiazzal <joel.piazzalungalerat@gmail.com>
2023-04-20 18:22:13 +0200

    📝 docs(texts.json): add aria label for play button ✨ feat(Home.tsx): add list of boards with play button and fetch boards from API

--- packages/frontend/src/pages/Home.tsx
+++ packages/frontend/src/pages/Home.tsx
@@ -1,3 +1,49 @@
+import MovieIcon from '@mui/icons-material/Movie';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import { useEffect, useState } from 'react';
+
+import { getBoards } from 'api/BoardApi';
+import texts from 'data/texts.json';
+import Board from 'types/board';
+
 export default function Home() {
-    return <p>Home</p>;
+    const [boards, setBoards] = useState<Board[]>([]);
+
+    const fetchBoards = async () => {
+        const boardsFetched = await getBoards();
+        setBoards(boardsFetched);
+    };
+
+    useEffect(() => {
+        fetchBoards();
+    }, []);
+    return (
+        <>
+            <List sx={{ maxWidth: 360, mx: 'auto' }}>
+                {boards.map((board) => (
+                    <ListItem
+                        key={board.id}
+                        secondaryAction={
+                            <IconButton edge="end" aria-label={texts['button.play.ariaLabel']} href="#">
+                                <PlayCircleIcon />
+                            </IconButton>
+                        }
+                    >
+                        <ListItemAvatar>
+                            <Avatar>
+                                <MovieIcon />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary={board.name} />
+                    </ListItem>
+                ))}
+            </List>
+        </>
+    );
 }

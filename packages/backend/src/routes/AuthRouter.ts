431cf93775851ec927ac46a135cded4b23c1f725
jpiazzal <joel.piazzalungalerat@gmail.com>
2023-06-02 16:06:52 +0200

    refactor(AuthRouter.ts): change username field to name field to match UserWithoutId type

--- packages/backend/src/routes/AuthRouter.ts
+++ packages/backend/src/routes/AuthRouter.ts
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 
 import { JWT_SECRET } from 'config/environment';
 import { SIGNUP_BASE_ROUTE } from 'constants/ApiConstants';
+import { UserWithoutId } from 'types/user';
 import { BadRequestException } from 'utils/exceptions';
 
 const AuthRouter: Router = Router();
@@ -10,13 +11,13 @@ const AuthRouter: Router = Router();
 const TOKEN_EXPIRATION = '1h';
 
 AuthRouter.post(SIGNUP_BASE_ROUTE, (request: Request, response: Response) => {
-    const { username, password } = request.body;
+    const { name, password }: UserWithoutId = request.body;
 
-    if (!username || !password) {
-        throw new BadRequestException('Username and password are required');
+    if (!name || !password) {
+        throw new BadRequestException('Fields name and password are required');
     }
 
-    const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: TOKEN_EXPIRATION });
+    const token = jwt.sign({ name }, JWT_SECRET, { expiresIn: TOKEN_EXPIRATION });
     return response.json({ token });
 });
 
--- packages/backend/tests/api/auth.test.ts
+++ packages/backend/tests/api/auth.test.ts
@@ -5,21 +5,22 @@ import { SIGNUP_ROUTE } from 'constants/ApiConstants';
 
 describe('Auth', () => {
     describe(`when POST ${SIGNUP_ROUTE}`, () => {
-        it('should return 400 if username is not provided', async () => {
+        it('should return 400 if name is not provided', async () => {
             const result = await request(app).post(SIGNUP_ROUTE).send({ password: 'password' }).expect(400);
-            expect(result.body).toEqual({ message: 'Username and password are required', status: 400 });
+            expect(result.body).toEqual({ message: 'Fields name and password are required', status: 400 });
         });
 
         it('should return 400 if password is not provided', async () => {
-            const result = await request(app).post(SIGNUP_ROUTE).send({ username: 'username' }).expect(400);
-            expect(result.body).toEqual({ message: 'Username and password are required', status: 400 });
+            const result = await request(app).post(SIGNUP_ROUTE).send({ name: 'name' }).expect(400);
+            expect(result.body).toEqual({ message: 'Fields name and password are required', status: 400 });
         });
 
         it('should return an object with property "token"', async () => {
             const result = await request(app)
                 .post(SIGNUP_ROUTE)
-                .send({ username: 'username', password: 'password' })
+                .send({ name: 'name', password: 'password' })
                 .expect(200);
+
             expect(result.body).toEqual({
                 token: expect.any(String)
             });
